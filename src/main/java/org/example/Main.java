12220046871f95b98d2e0bddbec03faab531e7c4
aleksdzie <aleksandra.dziedzic1@gmail.com>
2022-12-05 19:47:57 +0100

    Adding userDetails method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -13,34 +13,36 @@ public class Main {
         System.out.println("Wprowadź PESEL.");
         int pesel = scanner.nextInt();
 
-        System.out.println("Menu: \n 1 - Informacje o koncie \n 2 - Wpłata pieniędzy \n 3 - Wypłata pieniędzy \n 4 - Wyjście");
-
         User uzytkownik = new User(imie, nazwisko, pesel);
 
-//        int inputNumber=scanner.nextInt();
-//        switch(inputNumber){
-//            case 1:
-//                System.out.println(imie + nazwisko + pesel + login +  );
-//                break;
+        System.out.println("Menu: \n 1 - Informacje o koncie \n 2 - Wpłata pieniędzy \n 3 - Wypłata pieniędzy \n 4 - Wyjście");
+        System.out.println("Wybierz opcję.");
+        int opcja = scanner.nextInt();
+        switch (opcja) {
+            case 1:
+                System.out.println("Informacje o koncie:");
+                uzytkownik.uzytkownikDetails();
+                break;
 //            case 2:
 //                int saldo;
 //                System.out.println("Wprowadź kwotę, jaką chcesz wpłacić.");
-//                int kwota1=scanner.nextInt();
-//                    System.out.println("Kwota wpłaty: "+kwota1);
-//                    System.out.println("Aktualny stan konta: "+(saldo+=kwota1));
+//                int kwota1 = scanner.nextInt();
+//                System.out.println("Kwota wpłaty: " + kwota1);
+//                System.out.println("Aktualny stan konta: " + (saldo += kwota1));
 //                break;
 //            case 3:
 //                System.out.println("Wprowadź kwotę, jaką chcesz wypłacić.");
-//                int kwota2=scanner.nextInt();
-//                if(kwota2>saldo){
+//                int kwota2 = scanner.nextInt();
+//                if (kwota2 > saldo) {
 //                    System.out.println("Przekroczono limit konta!");
 //                }
-//                else{
-//                    System.out.println("Kwota wypłaty: "+kwota2);
-//                    System.out.println("Aktualny stan konta: "+(saldo-=kwota2));
-//                break;
-//            case 4:
-//                System.out.println("Case 4");
-//                break;
+//                else {
+//                    System.out.println("Kwota wypłaty: " + kwota2);
+//                    System.out.println("Aktualny stan konta: " + (saldo -= kwota2));}
+//                    break;
+//                case 4:
+//                    System.out.println("Case 4");
+//                    break;
+                }
+        }
     }
-}
--- src/main/java/org/example/User.java
+++ src/main/java/org/example/User.java
@@ -8,17 +8,20 @@ public class User {
     private int numerKonta;
     private double saldo;
 
-    public User(String imie, String nazwisko, int pesel){
-    this.imie=imie;
-    this.nazwisko=nazwisko;
-    this.pesel=pesel;
-    this.login=loginGenerator(imie, nazwisko);
-    this.saldo=100;
-    this.numerKonta=1;
+    public User(String imie, String nazwisko, int pesel) {
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+        this.pesel = pesel;
+        this.login = loginGenerator(imie, nazwisko);
+        this.saldo = 100;
+        this.numerKonta = 1;
     }
 
-    public String loginGenerator(String imie, String nazwisko){
-    return (imie.substring(0,3)+nazwisko.substring(0,3));
+    public String loginGenerator(String imie, String nazwisko) {
+        return (imie.substring(0, 3) + nazwisko.substring(0, 3));
     }
 
+    public void uzytkownikDetails(){
+        System.out.println("Numer konta: "+numerKonta+ "\n Imię: "+imie+"\n Nazwisko: "+nazwisko+"\n Login: "+login+"\n PESEL: "+pesel+"\n Saldo: "+saldo);
+    }
 }
